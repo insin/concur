47a6918fc07639813cfae58d11e3dd2f24a7fea2
Jonathan Buchanan <jonathan.buchanan@gmail.com>
2012-01-17 13:25:10 +0000

    [build] Added Google Closure Compiler compression step for browser version.

--- support/compile.js
+++ support/compile.js
@@ -2,6 +2,8 @@ var fs = require('fs')
   , path = require('path')
   , util = require('util')
 
+var request = require('request')
+
 var root = path.normalize(path.join(__dirname, '..'))
 
 // Config
@@ -18,7 +20,7 @@ var config = {
 var modules = []
 for (var name in config.modules) {
   var modulePath = path.join(root, config.modules[name])
-  console.log('compile: %s from %s', name, modulePath)
+  console.log('compile: %s (from %s)', name, modulePath)
   modules.push(
     util.format("require.define('%s', function(module, exports, require) {\n", name) +
     fs.readFileSync(modulePath) +
@@ -27,11 +29,51 @@ for (var name in config.modules) {
 }
 
 // Output
-fs.readFile(path.join(__dirname, 'template.js'), function(err, template) {
-  var outputPath = path.join(root, config.output)
-    , code = util.format(template.toString(), modules.join('\n'))
-  fs.writeFile(outputPath, code, function(err) {
-    if (err) throw error
-    console.log('create: %s', outputPath)
-  })
-})
+var template = fs.readFileSync(path.join(__dirname, 'template.js')).toString()
+  , code = util.format(template, modules.join('\n'))
+  , outputPath = path.join(root, config.output)
+console.log('create: %s', outputPath)
+fs.writeFileSync(outputPath, code)
+
+// Compress
+var compressedOutputPath = outputPath.replace('.js', '.min.js')
+
+function printErrorInfo(error) {
+  console.error('  L%s,%s [%s] %s',
+                error.lineno, error.charno, error.type, error.error)
+}
+
+function processClosureCompilerResponse(err, response, body) {
+  if (err) throw err
+  var result = JSON.parse(body)
+  console.log('statusCode: %s', response.statusCode)
+  if (result.hasOwnProperty('errors')) {
+    console.error('errors:')
+    result.errors.forEach(printErrorInfo)
+  }
+  if (result.hasOwnProperty('warnings')) {
+    console.error('warnings:')
+    result.warning.forEach(printErrorInfo)
+  }
+  console.log('statistics:')
+  console.log('  original size: %s (%s gzipped)',
+              result.statistics.originalSize, result.statistics.originalGzipSize)
+  console.log('  compressed size: %s (%s gzipped)',
+              result.statistics.compressedSize, result.statistics.compressedGzipSize)
+  if (!result.hasOwnProperty('errors')) {
+    console.log('compressed: %s', compressedOutputPath)
+    fs.writeFileSync(compressedOutputPath, result.compiledCode)
+  }
+}
+
+console.log('compressing %s...', outputPath)
+request.post({
+    url: 'http://closure-compiler.appspot.com/compile'
+  , form: {
+      js_code: code
+    , compilation_level: 'SIMPLE_OPTIMIZATIONS'
+    , output_format: 'json'
+    , output_info: ['compiled_code', 'errors', 'warnings', 'statistics']
+    }
+  }
+, processClosureCompilerResponse)
