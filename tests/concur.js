4863b8f84d6a3abe1ae44fe806d0c369e3b76783
Jonathan Buchanan <jonathan.buchanan@gmail.com>
2012-01-17 22:10:41 +0000

    Base objects should not be instanceof Concur. Tidied up test descriptions.

--- tests/concur.js
+++ tests/concur.js
@@ -1,6 +1,6 @@
 QUnit.module('Concur')
 
-QUnit.test('Concur.extend', 12, function() {
+QUnit.test('Concur.extend', 16, function() {
   var Widget = Concur.extend({
     constructor: function(attrs) {
       this.attrs = attrs || {}
@@ -19,26 +19,34 @@ QUnit.test('Concur.extend', 12, function() {
     inputType: 'text'
   })
 
+  var w = new Widget()
+  ok(!(w instanceof Concur), 'Base objects are not instanceof Concur')
+  ok(w instanceof Widget, 'Base objects are instanceof their own constructor')
+
   var w = new TextInput({style: 'color: red;'})
-  ok(w instanceof Widget, 'Is instanceof grandparent')
-  ok(w instanceof Input, 'Is instanceof parent')
-  deepEqual(w.attrs, {style: 'color: red;'}, 'Instance property set by grandparent constructor')
-  equal(w.inputType, 'text', 'Parent prototype property is overridden')
-  strictEqual(w.isHidden, false, 'Grandparent prototype property is overridden')
-  equal(w.render('foo', 'bar'), '<input type="text" name="foo" value="bar">', 'Parent method works')
+  ok(w instanceof Widget, 'Objects are instanceof their grandparent constructor')
+  ok(w instanceof Input, 'Objects are instanceof their parent constructor')
+  ok(w instanceof TextInput, 'Objects are instanceof their own constructor')
+  deepEqual(w.attrs, {style: 'color: red;'}, 'Ancestor constructors are called if none was provided while extending')
+  equal(w.inputType, 'text', 'Same-named ancestor (parent) prototype properties are overridden')
+  strictEqual(w.isHidden, false, 'Same-named ancestor (grandparent) prototype properties are overridden')
+  equal(w.render('foo', 'bar'), '<input type="text" name="foo" value="bar">',
+        'Ancestor (parent) methods are accessible via the prototype chain')
 
   var HiddenInput = Input.extend({
     inputType: 'hidden'
   , isHidden: true
   })
 
-  var w = new HiddenInput
-  ok(w instanceof Widget, 'Is instanceof grandparent')
-  ok(w instanceof Input, 'Is instanceof parent')
-  deepEqual(w.attrs, {}, 'Instance property set by grandparent constructor')
-  equal(w.inputType, 'hidden', 'Parent prototype property is overridden')
-  strictEqual(w.isHidden, true, 'Parent prototype property is overridden')
-  equal(w.render('foo', 'bar'), '<input type="hidden" name="foo" value="bar">', 'Parent method works')
+  var w = new HiddenInput()
+  ok(w instanceof Widget, 'Objects are instanceof their grandparent constructor')
+  ok(w instanceof Input, 'Objects are instanceof their parent constructor')
+  ok(w instanceof HiddenInput, 'Objects are instanceof their own constructor')
+  deepEqual(w.attrs, {}, 'Ancestor constructors are called if none was provided while extending')
+  equal(w.inputType, 'hidden', 'Same-named ancestor (parent) prototype properties are overridden')
+  strictEqual(w.isHidden, true, 'Same-named ancestor (parent) prototype properties are overridden')
+  equal(w.render('foo', 'bar'), '<input type="hidden" name="foo" value="bar">',
+        'Ancestor (parent) prototype properties are accessible via the prototype chain')
 })
 
 // Regression test for sane bahaviour when a constructor is not supplied on
@@ -51,10 +59,11 @@ QUnit.test('Concur.extend (without supplied constructor)', 5, function() {
       test: function() { return 'b' }
     })
 
-  var a = new A(), b = new B()
-  ok(a instanceof A, 'Is instance of self')
-  equal(a.test(), 'a', 'Prototype property is attached')
-  ok(b instanceof B, 'Is instance of self')
-  ok(b instanceof A, 'Is instance of parent')
-  equal(b.test(), 'b', 'Parent prototype property is overridden')
+  var a = new A()
+    , b = new B()
+  ok(a instanceof A, 'Objects are instanceof their own constructor')
+  equal(a.test(), 'a', 'Supplied prototype properties are accessible via the prototype chain')
+  ok(b instanceof B, 'Objects are instanceof their own constructor')
+  ok(b instanceof A, 'Objects are instanceof their parent constructor')
+  equal(b.test(), 'b', 'Same-named ancestor (parent) prototype properties are overridden')
 })
